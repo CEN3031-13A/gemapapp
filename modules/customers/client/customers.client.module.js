aa27db09c2a24670a74fe4eb263e585bafbe075c
Jesse Dubbs <jessetd@live.com>
2017-10-29 14:19:02 -0400

    Fixed Linter errors

--- modules/customers/client/customers.client.module.js
+++ modules/customers/client/customers.client.module.js
@@ -5,10 +5,10 @@
 }(ApplicationConfiguration));
 
 function myFunction() {
-    var x = document.getElementById("hide");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+  var x = document.getElementById('hide');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
 }
--- modules/customers/tests/server/customer.server.model.tests.js
+++ modules/customers/tests/server/customer.server.model.tests.js
@@ -17,8 +17,8 @@ var user,
 /**
  * Unit tests
  */
-describe('Customer Model Unit Tests:', function() {
-  beforeEach(function(done) {
+describe('Customer Model Unit Tests:', function () {
+  beforeEach(function (done) {
     user = new User({
       firstName: 'Full',
       lastName: 'Name',
@@ -28,7 +28,7 @@ describe('Customer Model Unit Tests:', function() {
       password: 'password'
     });
 
-    user.save(function() {
+    user.save(function () {
       customer = new Customer({
         name: 'Customer Name',
         user: user
@@ -38,28 +38,28 @@ describe('Customer Model Unit Tests:', function() {
     });
   });
 
-  describe('Method Save', function() {
-    it('should be able to save without problems', function(done) {
+  describe('Method Save', function () {
+    it('should be able to save without problems', function (done) {
       this.timeout(0);
-      return customer.save(function(err) {
+      return customer.save(function (err) {
         should.not.exist(err);
         done();
       });
     });
 
-    it('should be able to show an error when try to save without name', function(done) {
+    it('should be able to show an error when try to save without name', function (done) {
       customer.name = '';
 
-      return customer.save(function(err) {
+      return customer.save(function (err) {
         should.exist(err);
         done();
       });
     });
   });
 
-  afterEach(function(done) {
-    Customer.remove().exec(function() {
-      User.remove().exec(function() {
+  afterEach(function (done) {
+    Customer.remove().exec(function () {
+      User.remove().exec(function () {
         done();
       });
     });
