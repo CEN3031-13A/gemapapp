314f60a2de021de4d8331672db60e5cb6afbeb91
Kassandra Crompton <kcrompton@ufl.edu>
2017-10-29 17:52:23 -0400

    Tried working on javascript functions for sidebar and fixed formatting for the right sidebar

--- modules/customers/client/customers.client.module.js
+++ modules/customers/client/customers.client.module.js
@@ -12,3 +12,25 @@ function myFunction() {
     x.style.display = 'none';
   }
 }
+
+function getItemData(){
+	var px_tree = document.querySelector('px-tree');
+	var selectedData =px_tree.selectedMeta;
+	//(item:Object) -- reference to the selected item
+	var selectedShipment = selectedData.item;
+	console.log(_getSelectedMeta(selectedShipment));
+	//(path:Array) The path to the selected item as an array. 
+	//Begins with the top-most item in the graph and ends with the selected item. 
+	var shipmentHierarchy = selectedData.path;
+	//(selectedRoute:Array) Different way to get the same information
+	var shipmentHierarchy2 = px_tree.selectedRoute;
+	console.log("Selected Shipment: " + selectedShipment);
+	console.log("Shipment Hierarchy: " + shipmentHierarchy);
+	console.log("Shipment Hierarchy 2: " + shipmentHierarchy2);
+}
+
+function loading(){
+	// while(!document.ready());
+	var spinner = document.querySelector('px-spinner');
+	spinner.finished = true;
+}
