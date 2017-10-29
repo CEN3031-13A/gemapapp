72b75ff35e08dc4ef2d3fc6a9323e2d9c4f2bc12
Vishnu Sriram <vishnusriram@Vishnus-MacBook-Pro.local>
2017-10-28 23:06:10 -0400

    Merge branch 'develop' into vishnu
    # Conflicts:
    #	modules/core/client/css/core.css

--- modules/customers/server/routes/customers.server.routes.js
+++ modules/customers/server/routes/customers.server.routes.js
@@ -3,21 +3,20 @@
 /**
  * Module dependencies
  */
+var customersPolicy = require('../policies/customers.server.policy'),
+  customers = require('../controllers/customers.server.controller');
 
-var customers = require('../controllers/customers.server.controller');
-
-module.exports = function (app) {
-  // Articles collection routes
-  app.route('/api/customers')
+module.exports = function(app) {
+  // Customers Routes
+  app.route('/api/customers').all(customersPolicy.isAllowed)
     .get(customers.list)
     .post(customers.create);
 
-  // Single article routes
-  app.route('/api/customers/:customersId')
+  app.route('/api/customers/:customerId').all(customersPolicy.isAllowed)
     .get(customers.read)
     .put(customers.update)
     .delete(customers.delete);
 
-  // Finish by binding the article middleware
+  // Finish by binding the Customer middleware
   app.param('customerId', customers.customerByID);
 };
