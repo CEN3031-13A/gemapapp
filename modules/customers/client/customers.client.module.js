22ca63788fc85f2dd30000cc0f3626d86adb1dfd
Joshua John <joshua_john94@yahoo.com>
2017-10-29 18:32:35 -0400

    removed un-needed imports

--- modules/customers/client/customers.client.module.js
+++ modules/customers/client/customers.client.module.js
@@ -4,7 +4,7 @@
   app.registerModule('customers');
 }(ApplicationConfiguration));
 
-function myFunction() {
+function hideElement() {
     var x = document.getElementById("hide");
     if (x.style.display === "none") {
         x.style.display = "block";
