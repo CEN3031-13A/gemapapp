af838bd791061ece5b14ee5abfecde845435273d
Joshua John <joshua_john94@yahoo.com>
2017-11-19 14:00:04 -0500

    component sizing, general styling

--- modules/customers/client/customers.client.module.js
+++ modules/customers/client/customers.client.module.js
@@ -27,14 +27,18 @@ function myFunction() {
   var x = document.getElementById('hide');
   var y = document.getElementById('MAP_MARKERS');
   var z = document.getElementById('STEPS');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-    y.style.width = '63.5%';
-    z.style.width = '63vw';
-  } else {
+  if (x.style.display === 'block') {
     x.style.display = 'none';
-    y.style.width = '83.5%';
-    z.style.width = '83vw';
+    document.getElementById('MAP_MARKERS').className = "u-5/6";
+    document.getElementById('MAP_MARKERS').style.right = "0";
+    document.getElementById("STEPS").className = "u-5/6";
+    document.getElementById('STEPS').style.right = "0";
+  } else {
+    x.style.display = 'block';
+    document.getElementById('MAP_MARKERS').className = "u-4/6";
+    document.getElementById('MAP_MARKERS').style.right = "16.6666667%";
+    document.getElementById("STEPS").className = "u-4/6";
+    document.getElementById('STEPS').style.right = "16.6666667%";
   }
 }
 
